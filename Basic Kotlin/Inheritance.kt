0e705ba985d470f36850d9ddd3fa8c9b55dfec8c
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2021-07-07 03:03:22 +0900

    2회차

---
+++ Basic Kotlin/Inheritance.kt
@@ -0,0 +1,55 @@
+fun main(){
+    val squareCabin = SquareCabin(6)
+    with(squareCabin){
+    println("\nSquare Cabin \n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")        
+    }
+    
+    val roundHut = RoundHut(3)
+    with(roundHut){
+    println("\nRound Hut \n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")        
+    }
+
+    val roundTower = RoundTower(4)
+    with(roundTower){
+    println("\nRound Tower \n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")        
+    }
+
+    
+}
+
+abstract class Dwelling(private var residents : Int){
+
+   abstract val buildingMaterial : String
+   abstract val capacity : Int
+
+   fun hasRoom() : Boolean {
+       return residents < capacity
+   }
+}
+
+// class SquareCabin constructor(residents : Int) 와 같음
+class SquareCabin (residents : Int): Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+
+open class RoundHut(residents : Int) : Dwelling(residents){
+	override val buildingMaterial = "Straw"
+    override val capacity = 4
+}
+
+class RoundTower(
+    residents: Int,
+	val floors: Int = 2) : RoundHut(residents){
+	override val buildingMaterial = "Stone"
+    override val capacity = 4 * floors
+}
--- README.md
+++ README.md
@@ -3,9 +3,13 @@
     <a href="https://developer.android.com/kotlin?hl=ko">공식 자습서</a> 내용을 참고합니다
 </p>
 <p>
-    `21.07.05 <hr>
+    <hr>
+    `21.07.05 <br><br>
     View and Layout(HBD Card)<br>
     Object(Dice Roller)<br>
+    <hr>
+    `21.07.07 <br><br>
+    조건절의 리턴값을 사용하여 drawableResource를 세팅 (Dice Roller)<br>
+    상속 (abstract class, open class)<br>
+    with 키워드<br>
 </p>
-</body>
-</body>
